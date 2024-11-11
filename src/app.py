6dc84aeaaa487fa411383a16a2972803174f9d26
iragca <irag.chrisandrei@mailbox.org>
2024-11-11 14:15:10 +0000

    + User Interface, openai chat

---
+++ src/app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+store = {}  # memory is maintained outside the chain
+
+def get_session_history(session_id: str) -> InMemoryChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = InMemoryChatMessageHistory()
+    return store[session_id]
+
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+user_query = st.chat_input(f"Ask FiLEMon")
+
+# chain = RunnableWithMessageHistory(llm, get_session_history)
+# chain.invoke(
+#     "Hi I'm Bob.",
+#     config={"configurable": {"session_id": "1"}},
+# )  # session_id determines thread
